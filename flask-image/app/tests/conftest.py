eaf0aeb6a3bb5d3fbec512390836e7c7bb82e80d
Nick.Hopgood <nickhopgood@gmail.com>
2022-05-06 12:18:56 +0100

    added acceptance tests for remaining views.

--- flask-image/app/tests/conftest.py
+++ flask-image/app/tests/conftest.py
@@ -1,5 +1,6 @@
-from app.Models.association_table import Quotation, Order, NetRef, ProviderQuote
+from app.Models.association_table import Quotation, Order, NetRef, ProviderQuote, ProviderProduct, Customer
 import pytest
+from app import create_app
 
 @pytest.fixture(scope='module')
 def test_quote():
@@ -8,7 +9,7 @@ def test_quote():
 
 @pytest.fixture(scope='module')
 def test_provider_quote():
-    provider_quote = ProviderQuote('Virtual1', '12345')
+    provider_quote = ProviderQuote("1234567", 'Virtual 1')
     return provider_quote
 
 @pytest.fixture(scope='module')
@@ -17,9 +18,32 @@ def test_order():
     return order
 
 @pytest.fixture(scope='module')
+def test_customer():
+    customer = Customer("John Smith", "johnsmith@gmail.com")
+    return customer
+
+
+@pytest.fixture(scope='module')
+def test_provider_product():
+    provider_product = ProviderProduct('fibre', '100', '1000', 'BT', '250', '100.50', 'fibre everywhere', '1234567', '36', 'None')
+    return provider_product
+
+
+@pytest.fixture(scope='module')
 def test_association():
-    test_pricing = ProviderQuote("Virtual1", "12345")
+    test_provider_pricing = ProviderQuote("Virtual1", "12345")
     test_order = Order("Not Ordered")
+    test_customer = Customer("johnsmith@gmail.com", 'John Smith')
+    test_provider_product = ProviderProduct('fibre', '100', '1000', 'BT', '250', '100.50', 'fibre everywhere', '1234567',
+                                       '36', 'None')
     test_quote = Quotation("BT234AG", "12345")
-    assoc = NetRef(test_pricing, test_quote, test_order)
+    assoc = NetRef(test_pricing, test_quote, test_order, test_customer,test_provider_pricing, test_provider_product)
     return assoc
+
+@pytest.fixture(scope='module')
+def test_client():
+    flask_app = create_app('development')
+    with flask_app.test_client() as test_client:
+        #create application context
+        with flask_app.app_context():
+            yield test_client
--- flask-image/app/tests/unit/test_views.py
+++ flask-image/app/tests/unit/test_views.py
@@ -1,5 +1,3 @@
-
-
 def test_index_route(test_client):
     """
     GIVEN an HTTP GET to /
@@ -9,3 +7,43 @@ def test_index_route(test_client):
     response = test_client.get('/')
     assert response.status_code == 200
     assert b"Search existing quote" in response.data
+
+def test_view_pricing_route(test_client):
+    """
+    GIVEN an HTTP GET to /view_pricing
+    WHEN a response is received
+    THEN check the response code and initial html response
+    """
+    response = test_client.get('/view_pricing')
+    assert response.status_code == 200
+    assert b"Search existing quote" in response.data
+
+def test_view_quote_route(test_client):
+    """
+    GIVEN an HTTP GET to /new_quote
+    WHEN a response is received
+    THEN check the response code and initial html response
+    """
+    response = test_client.get('/view_quote')
+    assert response.status_code == 200
+    assert b"View all quotations" in response.data
+
+def test_new_quote_route(test_client):
+    """
+    GIVEN an HTTP GET to /new_quote
+    WHEN a response is received
+    THEN check the response code and initial html response
+    """
+    response = test_client.get('/new_quote')
+    assert response.status_code == 200
+    assert b"Request New Quote" in response.data
+
+def test_view_orders_route(test_client):
+    """
+    GIVEN an HTTP GET to /new_quote
+    WHEN a response is received
+    THEN check the response code and initial html response
+    """
+    response = test_client.get('/view_orders')
+    assert response.status_code == 200
+    assert b"View all orders" in response.data
